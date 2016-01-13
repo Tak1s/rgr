a93e3007fe76e00bd26faf6ad1fa202808404404
Tak1s <deedee604@gmail.com>
2016-01-13 16:53:21 +0200

    display operation list

--- rgr.py
+++ rgr.py
@@ -1,26 +1,27 @@
 
 class GetDetailInfo:
     def __init__(self, *args):
-        self.count_level = input("Введіть кількість ступенів: ")
-        self.dict_config = {a: {'lang':None, 'severity':None, 'isThread':False, 'isFask':False, 'faskSize':None} for a in range(int(self.count_level))}
-        # self.arr_level = []
-        # self.severity_level = []
-        # self.arr_thread = []
-        self.isFask = False
-        # self.arr_fask = []
-        self.arr_size_fask = []
+        self.dict_config = {0: {'diam':'6', 'isThread': False, 'leng': '1', 'faskSize': None, 'severity': '3.2', 'isFask': False}, 1: {'diam':'3', 'isThread': False, 'leng': '2', 'faskSize': None, 'severity': '3.2', 'isFask': False}}
+        # self.count_level = input("Введіть кількість ступенів: ")
+        # self.dict_config = {a: {'diam':None, 'leng':None, 'severity':None, 'isThread':False, 'isFask':False, 'faskSize':None} for a in range(int(self.count_level))}
+        # self.isFask = False
+        #
+        # self.get_leng_level(self.count_level)
+        # self.get_severity_level()
+        # self.get_thread()
+        # self.get_fask()
+        # self.get_size_fask()
 
-        self.get_lang_level(self.count_level)
-        self.get_severity_level()
-        self.get_thread()
-        self.get_fask()
-        self.get_size_fask()
-        print(str(self.dict_config))
+    def get_config(self):
+        return self.dict_config
 
-
-    def get_lang_level(self, count_level):
+    def get_diameter(self):
         for i in range(int(count_level)):
-            self.dict_config[i]['lang'] = input("Введіть довжину %s-го ступеню: " % (i+1))
+            self.dict_config[i]['diam'] = input("Введіть діаметр %s-го ступеню: " % (i+1))
+
+    def get_leng_level(self, count_level):
+        for i in range(int(count_level)):
+            self.dict_config[i]['leng'] = input("Введіть довжину %s-го ступеню: " % (i+1))
 
     def get_severity_level(self):
 
@@ -70,7 +71,6 @@ class GetDetailInfo:
                     break
                 elif int(val) <= int(self.count_level):
                     self.dict_config[int(val)-1]['isThread'] = True
-                    # self.arr_thread.append(int(val))
                 else:
                     break
             if self._zero is False and len(array_item) <= int(self.count_level):
@@ -99,7 +99,6 @@ class GetDetailInfo:
                     break
                 elif int(val) <= int(self.count_level):
                     self.dict_config[int(val)-1]['isFask'] = True
-                    # self.arr_fask.append(int(val))
                 else:
                     break
             if self._zero is False and len(array_item) <= int(self.count_level):
@@ -120,21 +119,11 @@ class GetDetailInfo:
             return
         def _get_one_item(index):
             item = input("Введіть розмір %s-ї фаски: " % (index+1))
-            # array_item = item.split(",")
-            # self._zero = False
-            # for val in array_item:
             if int(item) is 0:
                 print("більше немає фасок!")
-                # self._zero = True
-                # break
                 return 'break'
             elif int(item) <= int(self.count_level):
                 self.dict_config[index]['faskSize'] = item
-                # self.arr_size_fask.append(int(val))
-                # else:
-                #     break
-                # if self._zero is False and len(array_item) <= int(self.count_level):
-                #     _get_one_item()
 
         for i in self.dict_config:
             if self.dict_config[i]['isFask'] is False:
@@ -142,7 +131,33 @@ class GetDetailInfo:
             if _get_one_item(i) is 'break':
                 break
 
-GetDetailInfo()
+class DisplayMachinePperation:
+
+    def __init__(self, _detalConfig):
+        self.detalConfig = _detalConfig
+
+        self.list_operation = []
+        self.ra_tpl = {
+            '12.5':'Чорнове точіння довжини {0} діаметром {1}',
+            '6.3':'Чорнове, напівчистове точіння довжини {0} діаметром {1}',
+            '3.2':'Чорнове, напівчистове, чистове точіння довжини {0} діаметром {1}',
+            '1.6':'Чорнове, напівчистове, чистове точіння довжини {0} діаметром {1}',
+            '0.8':'Чорнове, напівчистове, чистове точіння, шліфування довжини {0} діаметром {1}'
+        }
+
+        for i in self.detalConfig:
+            _severity = self.detalConfig[i]['severity']
+            _diam = self.detalConfig[i]['diam']
+            _leng = self.detalConfig[i]['leng']
+            _tpl_operation = self.ra_tpl[_severity]
+            self.list_operation.append(_tpl_operation.format(_leng, _diam))
+
+        self.full_string = ('''005 Заготівельна\n010 Токарна\nПідрізати торець\n%s\n015 Контрольна''' % ("\n".join(self.list_operation)))
+
+        # print(str(self.detalConfig))
+        print(self.full_string)
 
 
+detalConfig = GetDetailInfo()
+DisplayMachinePperation(detalConfig.get_config())
 
